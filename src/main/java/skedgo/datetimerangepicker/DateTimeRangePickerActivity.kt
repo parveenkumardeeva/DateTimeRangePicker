725aff629cccd558e6942ec1bb0e4295ae9c0059
parveenkumaryadav <parveenkumardeeva@gmail.com>
2019-11-28 19:59:58 +0530

    Calendar Min/Max via configuration, from and to added instead of some other language, build tool version 29

--- src/main/java/skedgo/datetimerangepicker/DateTimeRangePickerActivity.kt
+++ src/main/java/skedgo/datetimerangepicker/DateTimeRangePickerActivity.kt
@@ -14,82 +14,86 @@ import java.util.*
 import android.content.Intent as Intent1
 
 class DateTimeRangePickerActivity : AppCompatActivity() {
-  companion object {
-    fun newIntent(
-            context: Context?,
-            timeZone: TimeZone?,
-            startTimeInMillis: Long?,
-            endTimeInMillis: Long?): Intent1 {
-      val intent = Intent1(context!!, DateTimeRangePickerActivity::class.java)
-      startTimeInMillis?.let { intent.putExtra(DateTimeRangePickerViewModel.KEY_START_TIME_IN_MILLIS, it) }
-      endTimeInMillis?.let { intent.putExtra(DateTimeRangePickerViewModel.KEY_END_TIME_IN_MILLIS, it) }
-      intent.putExtra(DateTimeRangePickerViewModel.KEY_TIME_ZONE, timeZone!!.id)
-      return intent
-    }
-  }
-
-  private val viewModel: DateTimeRangePickerViewModel by lazy {
-    DateTimeRangePickerViewModel(TimeFormatter(applicationContext))
-  }
-  private val binding: DateTimeRangePickerBinding by lazy {
-    DataBindingUtil.setContentView<DateTimeRangePickerBinding>(
-        this,
-        R.layout.date_time_range_picker
-    )
-  }
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-
-    viewModel.handleArgs(intent.extras)
-    binding.viewModel = viewModel
-
-    val toolbar = binding.toolbar
-    toolbar.inflateMenu(R.menu.date_time_range_picker)
-    toolbar.setNavigationOnClickListener { v -> finish() }
-    toolbar.setOnMenuItemClickListener { item ->
-      when {
-        item.itemId == R.id.dateTimeRangePickerDoneItem -> {
-          setResult(Activity.RESULT_OK, viewModel.createResultIntent())
-          finish()
+    companion object {
+        fun newIntent(
+                context: Context?,
+                timeZone: TimeZone?,
+                startTimeInMillis: Long?,
+                endTimeInMillis: Long?,
+                calendarMinTimeInMillis: Long?,
+                calendarMaxTimeInMillis: Long?): Intent1 {
+            val intent = Intent1(context!!, DateTimeRangePickerActivity::class.java)
+            startTimeInMillis?.let { intent.putExtra(DateTimeRangePickerViewModel.KEY_START_TIME_IN_MILLIS, it) }
+            endTimeInMillis?.let { intent.putExtra(DateTimeRangePickerViewModel.KEY_END_TIME_IN_MILLIS, it) }
+            calendarMinTimeInMillis?.let { intent.putExtra(DateTimeRangePickerViewModel.KEY_CALENDAR_MIN_TIME_IN_MILLIS, it) }
+            calendarMaxTimeInMillis?.let { intent.putExtra(DateTimeRangePickerViewModel.KEY_CALENDAR_MAX_TIME_IN_MILLIS, it) }
+            intent.putExtra(DateTimeRangePickerViewModel.KEY_TIME_ZONE, timeZone!!.id)
+            return intent
         }
-      }
-      true
     }
 
-    val calendarPickerView = binding.calendarPickerView
-    calendarPickerView.init(viewModel.minDate, viewModel.maxDate)
-        .inMode(CalendarPickerView.SelectionMode.RANGE)
-    viewModel.startDateTime.value?.let { calendarPickerView.selectDate(it.toDate()) }
-    viewModel.endDateTime.value?.let { calendarPickerView.selectDate(it.toDate()) }
-
-    calendarPickerView.setOnDateSelectedListener(object : CalendarPickerView.OnDateSelectedListener {
-      override fun onDateSelected(date: Date) {
-        viewModel.updateSelectedDates(calendarPickerView.selectedDates)
-      }
-
-      override fun onDateUnselected(date: Date) {
-        viewModel.updateSelectedDates(calendarPickerView.selectedDates)
-      }
-    })
-
-    binding.pickStartTimeView.setOnClickListener { v ->
-      showTimePicker(viewModel.startDateTime.value, viewModel.onStartTimeSelected)
+    private val viewModel: DateTimeRangePickerViewModel by lazy {
+        DateTimeRangePickerViewModel(TimeFormatter(applicationContext))
     }
-    binding.pickEndTimeView.setOnClickListener { v ->
-      showTimePicker(viewModel.endDateTime.value, viewModel.onEndTimeSelected)
+    private val binding: DateTimeRangePickerBinding by lazy {
+        DataBindingUtil.setContentView<DateTimeRangePickerBinding>(
+                this,
+                R.layout.date_time_range_picker
+        )
     }
-  }
 
-  private fun showTimePicker(
-      initialTime: DateTime,
-      listener: TimePickerDialog.OnTimeSetListener) {
-    TimePickerDialog(
-        this,
-        listener,
-        initialTime.hourOfDay,
-        initialTime.minuteOfHour,
-        DateFormat.is24HourFormat(this)
-    ).show()
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel.handleArgs(intent.extras!!)
+        binding.viewModel = viewModel
+
+        val toolbar = binding.toolbar
+        toolbar.inflateMenu(R.menu.date_time_range_picker)
+        toolbar.setNavigationOnClickListener { v -> finish() }
+        toolbar.setOnMenuItemClickListener { item ->
+            when {
+                item.itemId == R.id.dateTimeRangePickerDoneItem -> {
+                    setResult(Activity.RESULT_OK, viewModel.createResultIntent())
+                    finish()
+                }
+            }
+            true
+        }
+
+        val calendarPickerView = binding.calendarPickerView
+        calendarPickerView.init(viewModel.minDate.value, viewModel.maxDate.value)
+                .inMode(CalendarPickerView.SelectionMode.RANGE)
+        viewModel.startDateTime.value?.let { calendarPickerView.selectDate(it.toDate()) }
+        viewModel.endDateTime.value?.let { calendarPickerView.selectDate(it.toDate()) }
+
+        calendarPickerView.setOnDateSelectedListener(object : CalendarPickerView.OnDateSelectedListener {
+            override fun onDateSelected(date: Date) {
+                viewModel.updateSelectedDates(calendarPickerView.selectedDates)
+            }
+
+            override fun onDateUnselected(date: Date) {
+                viewModel.updateSelectedDates(calendarPickerView.selectedDates)
+            }
+        })
+
+        binding.pickStartTimeView.setOnClickListener { v ->
+            showTimePicker(viewModel.startDateTime.value, viewModel.onStartTimeSelected)
+        }
+        binding.pickEndTimeView.setOnClickListener { v ->
+            showTimePicker(viewModel.endDateTime.value, viewModel.onEndTimeSelected)
+        }
+    }
+
+    private fun showTimePicker(
+            initialTime: DateTime,
+            listener: TimePickerDialog.OnTimeSetListener) {
+        TimePickerDialog(
+                this,
+                listener,
+                initialTime.hourOfDay,
+                initialTime.minuteOfHour,
+                DateFormat.is24HourFormat(this)
+        ).show()
+    }
 }
